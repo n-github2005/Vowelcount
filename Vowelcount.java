f40818cd3b93f06243df5d9723164d14949f972b
n-github2005 <kadudulanamitha@gmail.com>
2025-01-30 22:25:46 +0530

    Add files via upload

---
+++ Vowelcount.java
@@ -0,0 +1,12 @@
+public class Vowelcount {
+    public static void main(String[] args) {
+        String str = "hello world";
+        int count = 0;
+        for (char ch : str.toLowerCase().toCharArray()) {
+            if ("aeiou".indexOf(ch) != -1) {
+                count++;
+            }
+        }
+        System.out.println("Number of vowels: " + count);
+    }
+}
